e65ada49757e7c4d9f6293115852a7e126073e96
zackluckyf <fanning.34@gmail.com>
2016-09-23 12:42:27 -0400

    template completed

--- src/app/component/component.js
+++ src/app/component/component.js
@@ -3,12 +3,20 @@
   angular.module('app')
     .component('listComponent', {
       bindings: {
-        message: '<'
+        results: '<',
+        query: '=',
+        search: '<',
+        time: '<'
       },
       url: '/component',
       templateUrl: 'app/component/component.html',
-      controller: function () {
-        this.message = 'Component Message';
+      controller: function (searchFactory) {
+        var vm = this;
+        this.results = [];
+        this.time = 500;
+        this.search = function (input) {
+          vm.results = searchFactory.getDatafn(input);
+        };
       }
     });
 }());
